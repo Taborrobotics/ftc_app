9442bc9823f9dcc89b6b41488736e4e520609bdb
Harper Mckerrow <hmckerrow19@taboracademy.org>
2018-11-30 16:47:39 -0500

    updates...probably not going to work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFinal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumFinal.java
@@ -4,7 +4,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.Range;
 
 
@@ -12,7 +11,7 @@ import com.qualcomm.robotcore.util.Range;
 
 public class MecanumFinal extends LinearOpMode {
 
-    private DcMotor m1, m2,m3,m4,lift,grab1,grab2;
+    private DcMotor m1, m2,m3,m4;
 
     private double rightPower = 0;
     private double leftPower = 0;
@@ -31,19 +30,7 @@ public class MecanumFinal extends LinearOpMode {
         m2 = hardwareMap.get(DcMotor.class, "Motor2");
         m3 = hardwareMap.get(DcMotor.class,"Motor3");
         m4 = hardwareMap.get(DcMotor.class,"Motor4");
-        lift = hardwareMap.get(DcMotor.class,"Lift_Motor");
-        grab1 = hardwareMap.get(DcMotor.class,"Grab1");
-        grab2 = hardwareMap.get(DcMotor.class,"Grab2");
 
-        grab1.setDirection(DcMotor.Direction.REVERSE);
-
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        grab1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        grab2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        grab1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        grab2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
@@ -58,100 +45,34 @@ public class MecanumFinal extends LinearOpMode {
 
 
 
-            double turn = Range.clip(gamepad1.right_stick_y,-0.5,0.5);
+            double turn = Range.clip(gamepad1.left_stick_y,-0.5,0.5);
+            double slide = Range.clip(gamepad1.right_stick_x, -0.5, 0.5);
 
             if (turn > 0){
-                m1.setPower(-turn);
-                m2.setPower(turn);
+                m1.setPower(turn);
+                m2.setPower(-turn);
                 m3.setPower(turn);
                 m4.setPower(-turn);
             }
             else if(turn < 0){
-                m1.setPower(turn);
-                m2.setPower(-turn);
+                m1.setPower(-turn);
+                m2.setPower(turn);
                 m3.setPower(-turn);
                 m4.setPower(turn);
             }
-            else{
+            if (slide > 0){
                 m1.setPower(-rightPower);
                 m2.setPower(-rightPower);
                 m3.setPower(leftPower);
                 m4.setPower(leftPower);
             }
-
-            if (gamepad1.a){
-                liftToPosition(0);
+            else if (slide < 0){
+                m1.setPower(rightPower);
+                m2.setPower(rightPower);
+                m3.setPower(-leftPower);
+                m4.setPower(-leftPower);
             }
-            if (gamepad1.b){
-                liftToPosition(-1069);
-            }
-            if (gamepad1.x){
-                liftToPosition(-2616);
-            }
-            if (gamepad1.y){
-                liftToPosition(-4837);
-            }
-
-            if ((gamepad1.right_trigger > 0) && (!opened)){
-                grabber(true);
-                opened = true;;
-            }
-            if ((gamepad1.right_trigger > 0) && opened){
-                grabber(false);
-                opened = false;
-            }
-
-
-
-
-
         }
     }
 
-    private void liftToPosition(int encoder_pos){
-        lift.setTargetPosition(encoder_pos);
-
-        double power = 0.0;
-        int currentPos = lift.getCurrentPosition();
-
-        if (currentPos > encoder_pos){
-            power = -0.5;
-        }
-        else{
-            power = 0.5;
-        }
-        while (lift.isBusy()){
-            lift.setPower(power);
-            telemetry.addData("Lift:","Moving...");
-            telemetry.update();
-        }
-
-        lift.setPower(0);
-
-
-    }
-
-    private void grabber(boolean open){
-        double power = 0.0;
-        if (open){
-            grab1.setTargetPosition(300);
-            grab2.setTargetPosition(300);
-
-            power = 0.5;
-        }
-        else{
-            grab1.setTargetPosition(0);
-            grab2.setTargetPosition(0);
-
-            power = -0.5;
-        }
-
-        while (grab1.isBusy() && grab2.isBusy()){
-            grab1.setPower(power);
-            grab2.setPower(power);
-        }
-
-        grab1.setPower(0);
-        grab2.setPower(0);
-    }
 }
